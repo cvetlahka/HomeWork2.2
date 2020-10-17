a59b6b7cf153357d48dfa5b6828be4459def9480
Safonova Svetlana <Safonova_s@icloud.com>
2020-10-17 17:46:15 +0300

    дополнение

--- HomeWork2.2/ViewController.swift
+++ HomeWork2.2/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet var mainDisplay: UIView!
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sliderRed.value = 0.5
+        sliderRed.minimumValue = 0
+        sliderRed.maximumValue = 1
+        
+        sliderBlue.minimumValue = 0
+        sliderBlue.maximumValue = 1
+        
+        sliderGreen.minimumValue = 0
+        sliderGreen.maximumValue = 1
+
+        redValue.text = String(sliderRed.value)
+        greenValue.text = String(sliderGreen.value)
+        blueValue.text = String(sliderBlue.value)
     }
 
-
+    @IBAction func sliderRedAction() {
+        redValue.text = String(format:"%.2f", sliderRed.value)
+    }
+    
+    @IBAction func sliderGreenAction() {
+        greenValue.text = String(format:"%.2f", sliderGreen.value)
+    }
+    
+    @IBAction func sliderBlueAction() {
+        blueValue.text = String(format:"%.2f", sliderBlue.value)
+    }
 }
 
